ff1e331d14ba4e0b1de0eb7c2136bf2b555351ab
inaki-amatria <inaki.amatria@appentra.com>
2026-02-20 10:05:56 +0000

    Run `npx astro add tailwind`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'astro/config';
 
 export default defineConfig({
   site: 'https://inaki-amatria.github.io',
+  vite: {plugins: [tailwindcss()]},
   trailingSlash: 'never',
   compressHTML: true,
-  output: 'static'
+  output: 'static',
 });
