9dc9b72d352abc0921abc3e2cac50fc8d6e32dea
inaki-amatria <inaki.amatria@appentra.com>
2026-02-20 09:55:22 +0000

    Set up GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
 import {defineConfig} from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://inaki-amatria.github.io',
+  trailingSlash: 'never',
+  compressHTML: true,
+  output: 'static'
+});
