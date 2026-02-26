bb80d468fbe7b650f8041f981a793a9e589d5988
inaki-amatria <inaki.amatria@appentra.com>
2026-02-26 10:28:45 +0000

    Run `npx astro add preact`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
+import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'astro/config';
 
 export default defineConfig({
   site: 'https://inaki-amatria.github.io',
   vite: {plugins: [tailwindcss()]},
+  integrations: [preact()],
   trailingSlash: 'never',
   compressHTML: true,
   output: 'static',
