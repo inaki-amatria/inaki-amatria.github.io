53439cb92584d56156424f40dc6c66958bfdaade
inaki-amatria <inaki.amatria@appentra.com>
2026-02-27 11:16:04 +0000

    Remove `Button`

--- src/components/pages/contacto/ContactFormIsland.tsx
+++ src/components/pages/contacto/ContactFormIsland.tsx
@@ -55,7 +55,7 @@ const ContactFormIsland: FunctionalComponent = () => {
         class="border border-gray-200 rounded-xl p-3 text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600"
       />
 
-      <button type="submit" class="px-4 py-2 rounded-full font-bold text-base text-center shadow-md transition-all duration-300 hover:ring-2 active:scale-95 bg-blue-900 text-white hover:ring-blue-600/50 hover:shadow-lg w-fit cursor-pointer">
+      <button type="submit" class="button button-primary w-fit cursor-pointer">
         Enviar mensaje
       </button>
 
